0ffc0282a8d9ffe3e1ce7c358f246c81296676e0
Fanlanou <bagomedovjusuf@gmail.com>
2023-05-07 21:17:07 +0200

    fixed

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -18,20 +18,16 @@ int	ft_find_next_prime(int nb)
 {
 	if (nb <= 1)
 		return (2);
-	while (nb > 1)
-	{
-		if (ft_is_prime(nb) == 1)
-			return (nb);
+	while (!ft_is_prime(nb))
 		nb++;
-	}
-	return (0);
+	return (nb);
 }
 
 #include <stdio.h>
 int	main()
 {
-	printf("1--->%d\n",ft_find_next_prime(1));
-	printf("0--->%d\n",ft_find_next_prime(112));
+	printf("-1--->%d\n",ft_find_next_prime(-1));
+	printf("112--->%d\n",ft_find_next_prime(112));
 	printf("55-->%d\n",ft_find_next_prime(55));
         printf("7--->%d\n",ft_find_next_prime(7));
 	printf("11-->%d\n",ft_find_next_prime(11));
